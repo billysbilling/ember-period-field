84372a020de594c22864954e2d40b877ee5cda49
Sebastian Seilund <sebastian@seilund.com>
2015-02-06 16:25:59 -0800

    Added selectorAlign option

--- src/js/period-field-mixin.js
+++ src/js/period-field-mixin.js
@@ -1,17 +1,20 @@
 module.exports = Em.Mixin.create({
     value: null,
 
+    selectorAlign: 'left',
+
     click: function() {
         this.showSelector();
     },
-    
+
     showSelector: function() {
         var self = this,
             selector = this.get('selector');
         if (!selector) {
             selector = this.container.lookup('component:periodSelector');
             this.set('selector', selector);
-            selector.set('value', this.get('value'));
+            selector.set('value', this.get('value'))
+                .set('align', this.get('selectorAlign'))
             selector.on('select', this, this.selectorDidSelect);
             selector.one('willDestroyElement', function() {
                 self.set('selector', null);
@@ -19,7 +22,7 @@ module.exports = Em.Mixin.create({
             selector.show(this);
         }
     },
-    
+
     hideSelector: function() {
         var selector = this.get('selector');
         if (selector) {
